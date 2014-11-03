cbb1c2e8917cabc4bc54201361bc91a2fb0bac30
mala23 <mail@mala23.me>
2014-11-03 17:35:37 +0000

    user can create new restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -24,6 +24,19 @@ describe 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
 
+  context 'creating restaurants' do
+    it 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+
   end
+
+  end
+
 
 end
