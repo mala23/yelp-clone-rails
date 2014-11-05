c401c08f4279a8ea86dc7b0020b27ab76f419aca
mala23 <mail@mala23.me>
2014-11-05 12:15:24 +0000

    validate restaurant name uniqueness

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Restaurant, :type => :model do
+
   it 'is valid with a name shorter then three characters' do
     restaurant = Restaurant.new(name: 'kf')
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Wank Haus")
+    restaurant = Restaurant.new(name: "Wank Haus")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
