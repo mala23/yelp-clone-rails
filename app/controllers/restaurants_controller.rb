5844580c780f436f1e2bd4b89bb642c987ca3e8d
mala23 <mail@mala23.me>
2014-11-20 20:16:00 +0000

    fix spelling bug (thx spike) and make it work

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -43,7 +43,7 @@ class RestaurantsController < ApplicationController
     private
 
     def restaurant_params
-      params.require(:restaurant).permit(:name)
+      params.require(:restaurant).permit(:name, :image)
     end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thump => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def average_rating
